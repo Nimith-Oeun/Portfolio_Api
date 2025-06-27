80aa7d4cdf563db5a4a6e8779f556ce18cfb6d2d
(Nimith-Oeun) <oeunnimith@gmail.com>
2025-06-27 21:14:15 +0700

    Buils War file

--- src/main/java/com/personal/portfolio_api/PortfolioApiApplication.java
+++ src/main/java/com/personal/portfolio_api/PortfolioApiApplication.java
@@ -2,12 +2,19 @@ package com.personal.portfolio_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class  PortfolioApiApplication {
+public class  PortfolioApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PortfolioApiApplication.class);
     }
 
 }
